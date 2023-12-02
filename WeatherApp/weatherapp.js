373ae6e949f2697513d8fb8964e9f5868b68f999
kylejgallagher <111585427+kylejgallagher@users.noreply.github.com>
2023-12-02 20:46:05 +0900

    Update weatherapp.js

--- WeatherApp/weatherapp.js
+++ WeatherApp/weatherapp.js
@@ -3,7 +3,7 @@ import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm"
 // https://api.open-meteo.com/v1/forecast?current=temperature_2m,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime
 
 export function getWeather(lat, lon, timezone) {
-  return axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", {
+  return axios.get("https://api.open-meteo.com/v1/forecast?current=temperature_2m,apparent_temperature,precipitation,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&timeformat=unixtime", {
     params: {
       latitude: lat,
       longitude: lon,
